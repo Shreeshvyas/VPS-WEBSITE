801fbc7bf76abaf30f0c357fa51afc9b76362f44
shreeshvyas <shreesh@sphinx-solution.com>
2026-06-27 22:40:13 +0530

    Revert homepage override and restore school app homepage

--- vyas_school_project/urls.py
+++ vyas_school_project/urls.py
@@ -2,13 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/javascript'), name='sw.js'),
-    path('manifest.json', TemplateView.as_view(template_name='manifest.json', content_type='application/json'), name='manifest.json'),
-    path('', include('homepage.urls')),
     path('', include('school.urls', namespace='school')),
 ]
 
